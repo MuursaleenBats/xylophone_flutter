55ba4ae93db82cc90c86e6596eb6db414048e9d4
MuursaleenBats <mursaleenbatatawala@gmail.com>
2023-07-01 21:41:28 +0530

    Sound Added and UI updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:word_generator/word_generator.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(
@@ -8,13 +8,44 @@ void main() {
 }
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int n) async {
+    final player = AudioPlayer();
+    await player.play(AssetSource('note${n}.wav'));
+  }
+
+  Expanded buildKey(Color color, int n) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          playSound(n);
+        },
+        child: Text(" "),
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(color),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(Colors.red, 1),
+              buildKey(Colors.orange, 2),
+              buildKey(Colors.yellow, 3),
+              buildKey(Colors.teal, 4),
+              buildKey(Colors.green, 5),
+              buildKey(Colors.blue, 6),
+              buildKey(Colors.purple, 7),
+            ],
+          ),
         ),
       ),
     );
